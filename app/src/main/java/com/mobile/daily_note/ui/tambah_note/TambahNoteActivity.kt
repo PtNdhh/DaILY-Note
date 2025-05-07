b5c51f0f7c6763427e6428745b4edae031115f69
PutuIndah <puspitadewi967@gmail.com>
2025-05-07 21:23:37 +0800

    test1

---
+++ app/src/main/java/com/mobile/daily_note/ui/tambah_note/TambahNoteActivity.kt
@@ -0,0 +1,21 @@
+package com.mobile.daily_note.ui.tambah_note
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mobile.daily_note.R
+
+class TambahNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tambah_note)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

d317501cfae621e98b14596ada6ac37ad114ddef
F1D022054_Indira Felista Sanis S <120652051+indirasanis19@users.noreply.github.com>
2025-06-28 20:19:51 +0800

    indira - Pembersihan File part 2

--- app/src/main/java/com/mobile/daily_note/ui/home/ui/help/HelpViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.mobile.daily_note.ui.home.ui.help
-
-import androidx.lifecycle.ViewModel
-
-class HelpViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/mobile/daily_note/ui/main_activity/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.mobile.daily_note.ui.main_activity
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import com.mobile.daily_note.data.local.UserPreference
-
-class MainViewModel(private val pref: UserPreference) : ViewModel(){
-    fun getSession() = pref.getSession().asLiveData()
-}
